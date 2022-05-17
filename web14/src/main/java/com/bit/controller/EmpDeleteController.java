537099af9d5b2c34b033702358d23352c9c94263
bit01class <bit01class@gmail.com>
2022-05-17 16:17:54 +0900

    api delete service

---
+++ web14/src/main/java/com/bit/controller/EmpDeleteController.java
@@ -0,0 +1,32 @@
+package com.bit.controller;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.bit.util.BitMy;
+
+public class EmpDeleteController extends HttpServlet {
+
+	@Override
+	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		int empno=Integer.parseInt(req.getParameter("empno"));
+		String sql="delete from emp where empno="+empno;
+		try(
+			Connection conn=BitMy.getConnection();
+			Statement stmt=conn.createStatement();
+		){
+			stmt.executeUpdate(sql);
+			resp.setStatus(HttpServletResponse.SC_OK);
+		} catch (SQLException e) {
+			e.printStackTrace();
+			resp.setStatus(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
+		}
+	}
+}
