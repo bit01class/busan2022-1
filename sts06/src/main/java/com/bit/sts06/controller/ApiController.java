6c4fe55842abcbba31a1912b4713dd6b00d019cc
bit01class <bit01class@gmail.com>
2022-06-09 16:57:42 +0900

    use Gson

--- sts06/src/main/java/com/bit/sts06/controller/ApiController.java
+++ sts06/src/main/java/com/bit/sts06/controller/ApiController.java
@@ -8,12 +8,21 @@ import javax.servlet.http.HttpServletResponse;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
 import com.bit.sts06.domain.EmpDao;
 import com.bit.sts06.domain.EmpVo;
+import com.google.gson.Gson;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonObject;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 @RequestMapping("/api")
 public class ApiController {
@@ -23,21 +32,68 @@ public class ApiController {
 	@RequestMapping(value = "/emp",method = RequestMethod.GET)
 	public void list(HttpServletResponse res) throws Exception {
 		res.setContentType("application/json; charset=utf-8");
-		PrintWriter out = res.getWriter();
-		out.print("{\"root\":[");
-		boolean boo=true;
+//		PrintWriter out = res.getWriter();
+//		out.print("{\"root\":[");
+//		boolean boo=true;
+//		for(EmpVo bean: empDao.findAll()) {
+//			if(boo) {
+//				boo=false;
+//			}else {
+//				out.print(',');
+//			}
+//			out.print("{\"empno\":"+bean.getEmpno()+",\"ename\":\""
+//					+bean.getEname()+"\",\"hiredate\":\""
+//					+(bean.getHiredate()==null?"":bean.getHiredate().getTime())+"\"}");
+//			
+//		}
+//		out.print("]}");
+		JsonArray arr=new JsonArray();
 		for(EmpVo bean: empDao.findAll()) {
-			if(boo) {
-				boo=false;
-			}else {
-				out.print(',');
-			}
-			out.print("{\"empno\":"+bean.getEmpno()+",\"ename\":\""
-					+bean.getEname()+"\",\"hiredate\":\""
-					+(bean.getHiredate()==null?"":bean.getHiredate().getTime())+"\"}");
-			
+			JsonObject json = new JsonObject();
+			json.addProperty("empno", bean.getEmpno());
+			json.addProperty("ename", bean.getEname());
+			json.addProperty("hiredate", bean.getHiredate()==null?"":bean.getHiredate().getTime()+"");
+			arr.add(json);
 		}
-		out.print("]}");
+		res.getWriter().print(arr.toString());
+	}
+	
+	@RequestMapping(value = "/emp",method = RequestMethod.POST)
+	public void add(HttpServletResponse res,@ModelAttribute EmpVo bean) throws Exception {
+		log.info("add api param:"+bean);
+		empDao.insertOne(bean);
+		res.setStatus(HttpServletResponse.SC_OK);
+	}
+	
+	@RequestMapping(value="/emp/{empno}",method = RequestMethod.GET)
+	public void detail(@PathVariable("empno")int empno,HttpServletResponse res) throws Exception {
+		EmpVo bean=empDao.findOne(empno);
+		res.setStatus(HttpServletResponse.SC_OK);
+//		res.getWriter().print("{\"empno\":"+bean.getEmpno()+",\"ename\":\""
+//				+bean.getEname()+"\",\"job\":\""+bean.getJob()+"\",\"sal\":"+bean.getSal()+"}");
+		
+//		JsonObject json=new JsonObject();
+//		json.addProperty("empno", bean.getEmpno());
+//		json.addProperty("ename", bean.getEname());
+//		json.addProperty("sal", bean.getSal());
+//		json.addProperty("job", bean.getJob());
+//		res.getWriter().print(json.toString());
+		
+		Gson gson=new Gson();
+		String msg=gson.toJson(bean);
+		res.getWriter().print(msg);
+	}
+	
+	@RequestMapping("/test")
+	public void testApi(HttpServletResponse res) throws IOException {
+		JsonArray arr=new JsonArray();
+		JsonObject json = new JsonObject();
+		json.addProperty("empno", 1111);
+		arr.add(json);
+		json = new JsonObject();
+		json.addProperty("empno", 2222);
+		arr.add(json);
+		res.getWriter().print(arr.toString());
 	}
 }
 
--- sts06/src/main/java/com/bit/sts06/controller/MainController.java
+++ sts06/src/main/java/com/bit/sts06/controller/MainController.java
@@ -3,9 +3,14 @@ package com.bit.sts06.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class MainController {
 
 	@RequestMapping("/list")
-	public void list() {}
+	public void list() {
+		log.info("list page");
+	}
 }
